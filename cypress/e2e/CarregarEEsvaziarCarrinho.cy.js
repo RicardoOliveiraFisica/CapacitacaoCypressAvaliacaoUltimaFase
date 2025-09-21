1b39812bf6bbbbc01de9bb9690fa4c83e4452f17
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2025-09-21 01:12:42 -0300

    load and empty cart

---
+++ cypress/e2e/CarregarEEsvaziarCarrinho.cy.js
@@ -0,0 +1,53 @@
+/// <reference types='cypress' />
+//import { before } from 'mocha'
+import Login from '../support/Pages/Login'
+import Home from '../support/Pages/Home'
+import CategoryProducts from '../support/Pages/CategoryProducts'
+
+
+describe('Carregar e Esvaziar carrinho', function(){
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // impede falha por erro de JS da página
+      return false
+    })
+
+    beforeEach(function(){
+
+        cy.fixture('credenciaisFixture').then((dados)=>{
+                this.credenciaisExt = dados
+        }) 
+        
+        Home.acessarHome()
+        Home.verificarSeLogado().then((logado) => {
+                if (!logado) {      
+                        Login.acessarLogin()
+                        cy.url().should('include', 'automationexercise')                          
+                        cy.realizarLogin(this.credenciaisExt.email.email_valido,
+                                this.credenciaisExt.passwords.password_valido)
+                }
+        })           
+
+              
+    })
+    
+    for (let item=1; item<5; item++){
+        it(`Inserir item nº "${item}"`, function(){    
+                CategoryProducts.selecionarCategoriaAleatoria()
+                CategoryProducts.clicarNoProdutoAleatorio()
+                CategoryProducts.clicarEmContinuarComprando()
+        })
+    }
+
+    it("Verificar carrinho com itens", function(){
+        CategoryProducts.clicarEmVerCarrinho()
+    })
+
+    it("Esvaziar carrinho", function(){
+        CategoryProducts.clicarEmVerCarrinho()
+        CategoryProducts.esvaziarCarrinho()
+        CategoryProducts.carrinhoEsvaziadoComSucesso()
+    })
+
+
+
+})
